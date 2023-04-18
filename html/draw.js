b4620eb7ee481718cd15749a0d63898a74465226
sara-gaballa <saragaballa2002@gmail.com>
2023-04-18 02:08:58 +0200

    drawing

--- html/draw.js
+++ html/draw.js
@@ -1,15 +1,17 @@
 let nodenumber=0;
 const nodes = [];
-let node;
+let node, layer;
 let from, to, gain;
-const layer = new Konva.Layer();
-
+let map = new Map();
 window.onload = function() {
-  stage = new Konva.Stage({
+    stage = new Konva.Stage({
     container: "drawing-board",
     width: window.innerWidth,
     height: window.innerHeight,
   });
+//  const signalFlowGraph = new SignalFlowGraph(4);
+
+  layer = new Konva.Layer();
   const addNodeButton = document.querySelector('#node');
   addNodeButton.addEventListener('click', () => {
     drawNode();
@@ -99,42 +101,92 @@ function drawNode() {
     layer.add(node);
     stage.add(layer);
 }
+function drawLine() {
+    let shape1;
+    let shape2;
+    let first_pointx;
+    let first_pointy;
+    let second_pointx;
+    let second_pointy;
+    
+    if (from.charAt(0) === 'X' && to.charAt(0) === 'X') {
+      const index1 = parseInt(from.charAt(1));
+      const index2 = parseInt(to.charAt(1));
+      
+      if (!map.has(index1)) {
+        map.set(index1, 0);
+      }
+      
+      if (!map.has(index2)) {
+        map.set(index2, 0);
+      }
+      
+      let firstValue = map.get(index1);
+      let secondValue = map.get(index2);
+      
+      // Increment values
+      map.set(index1, firstValue + 1);
+      map.set(index2, secondValue + 1);
+      
+      console.log(map);
+      shape1 = nodes[index1];
+      shape2 = nodes[index2];
+      first_pointx = (shape1.attrs.x * 2 + shape1.attrs.width) / 2;
+      first_pointy = (shape1.attrs.y * 2 + shape1.attrs.height) / 2;
+      second_pointx = (shape2.attrs.x * 2 + shape2.attrs.width) / 2;
+      second_pointy = (shape2.attrs.y * 2 + shape2.attrs.height) / 2;
 
-function drawLine(){
-  let shape1;
-  let shape2;
-  let first_pointx;
-  let first_pointy;
-  let second_pointx;
-  let second_pointy;
-  if(from.charAt(0) == 'X' && to.charAt(0)=='X'){
-    shape1 = nodes[parseInt(from.charAt(1))];
-    shape2 = nodes[parseInt(to.charAt(1))];
-  } else {
-    from.value='';
-    to.value='';
-    return;
+      if((map.get(index1) == 1 || map.get(index2) == 1) && (index2 - index1) == 1 ){
+        let arrow = new Konva.Arrow({
+          points: [first_pointx, first_pointy, second_pointx, second_pointy],
+          stroke: 'black',
+          fill: 'black',
+          tension: 0.5 // set tension to a non-zero value to create a curve
+          
+        });
+        let text = new Konva.Text({
+            text: gain,
+            x: (first_pointx + second_pointx) / 2,
+            y: (first_pointy + second_pointy) / 2,
+            fontSize: 20,
+            fill: 'blue',
+            listening: false // disable event listening to avoid interfering with the arrow shape
+          });
+        layer.add(arrow);
+        layer.add(text);
+        from.value = '';
+        to.value = '';
+        layer.batchDraw();
+         console.log("case 1");
+      }else{
+        let curvePx =  (first_pointx + second_pointx)/2 
+        let curvePy =  (first_pointy + second_pointy)/2  + 50 * Math.max(map.get(index1),map.get(index2))
+
+        let arrow = new Konva.Arrow({
+            points: [first_pointx, first_pointy,curvePx ,curvePy ,second_pointx, second_pointy],
+            stroke: 'black',
+            fill: 'black',
+            tension: 0.5 // set tension to a non-zero value to create a curve
+          });
+          let text = new Konva.Text({
+            text: gain,
+            x: curvePx,
+            y: curvePy,
+            fontSize: 20,
+            fill: 'blue',
+            listening: false // disable event listening to avoid interfering with the arrow shape
+          });
+          layer.add(arrow);
+          layer.add(text);
+          from.value = '';
+          to.value = '';
+          layer.batchDraw();
+      }
+      
+
+    } else {
+      from.value = '';
+      to.value = '';
+      return;
+    }
   }
-    first_pointx = (shape1.attrs.x*2+shape1.attrs.width)/2;
-    first_pointy = (shape1.attrs.y*2+shape1.attrs.height)/2;
-    second_pointx = (shape2.attrs.x*2+shape2.attrs.width)/2;
-    second_pointy = (shape2.attrs.y*2+shape2.attrs.height)/2;
-  let arrow = new Konva.Arrow({
-    points: [first_pointx,first_pointy,second_pointx,second_pointy],
-    stroke: 'black',
-    fill: 'black'
-  });
-  let text = new Konva.Text({
-    text: gain,
-    x: (first_pointx + second_pointx) / 2,
-    y: (first_pointy + second_pointy) / 2,
-    fontSize: 20,
-    fill: 'blue',
-    listening: false // disable event listening to avoid interfering with the arrow shape
-  });
-  layer.add(arrow);
-  layer.add(text);
-  from.value='';
-  to.value='';
-  layer.batchDraw();
-}
